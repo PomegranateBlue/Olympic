94e44d3dbf6eac45cc4b3fa4de48e7c4848140fd
bluepome98 <wpdn1998@gmail.com>
2025-01-23 10:53:01 +0900

    테이블 css 완료?

--- src/MedalList.jsx
+++ src/MedalList.jsx
@@ -3,24 +3,24 @@ import "./MedalList.css";
 const MedalList = ({ rows, deleteMedalInfo }) => {
   return (
     <div className="TableContainer">
-      <table>
+      <table className="TableContent">
         <thead>
-          <tr className="listHeader">
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>액션</th>
+          <tr className="tableHeader">
+            <th className="tableHeaderCell">국가명</th>
+            <th className="tableHeaderCell">금메달</th>
+            <th className="tableHeaderCell">은메달</th>
+            <th className="tableHeaderCell">동메달</th>
+            <th className="tableHeaderCell">액션</th>
           </tr>
         </thead>
         <tbody>
           {rows.map((row) => (
             <tr key={row.id} className="listDetail">
-              <td>{row.country}</td>
-              <td>{row.gold}</td>
-              <td>{row.silver}</td>
-              <td>{row.copper}</td>
-              <td>
+              <td className="tableBodyCell">{row.country}</td>
+              <td className="tableBodyCell">{row.gold}</td>
+              <td className="tableBodyCell">{row.silver}</td>
+              <td className="tableBodyCell">{row.copper}</td>
+              <td className="tableBodyCell">
                 <button id="deleteBtn" onClick={() => deleteMedalInfo(row.id)}>
                   삭제
                 </button>
